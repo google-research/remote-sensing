af89149336365283a18599466c3d5a10007927f3
Genady Beryozkin <genady@google.com>
2026-08-19 16:50:56 +0300

    Update minor version.
    PiperOrigin-RevId: 967184380

--- remote_sensing/__init__.py
+++ remote_sensing/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""remote_sensing API."""
+"""Earth AI Remote Sensing API."""
 
 # A new PyPI release will be pushed every time `__version__` is increased.
 # When changing this, also update the CHANGELOG.md.
-__version__ = '0.1.0'
+__version__ = '0.1.1'
